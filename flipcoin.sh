6ceba6a292c5de47feb111c54f42729c070e9ca7
adnanshaik <shaikhadnan6608@gmail.com>
2020-09-10 07:28:28 +0530

    Added:loop through multiple times of flipping a coin that shows head or tail wons

---
+++ flipcoin.sh
@@ -0,0 +1,28 @@
+
+
+#!/bin/bash -x
+t=0
+h=0
+n=0
+while [ $n -le 5 ]
+do
+a=$(( RANDOM%2 ))
+
+if [ $a -eq 0 ]
+then
+        ((h++))
+        if [ $h -eq 3 ]
+        then
+                echo "head wins"
+        fi
+
+else
+        ((t++))
+        if [ $t -eq 3 ]
+        then
+        echo "tail wins"
+        fi
+fi
+((n++))
+done
+
