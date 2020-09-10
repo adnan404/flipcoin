a2f0d4b7c0d743109327d4f56a01d513802049ee
adnanshaik <shaikhadnan6608@gmail.com>
2020-09-10 07:37:05 +0530

    Added:Modify the earlier UC to continue until either head or tails reaches to 21

---
+++ flipcoin.sh
@@ -0,0 +1,27 @@
+t=0
+h=0
+
+until [ $h -eq 21 ] || [ $t -eq 21 ]
+do
+a=$(( RANDOM%2 ))
+
+if [ $a -eq 0 ]
+then
+        ((h++))
+        if [ $h -eq 21 ]
+        then
+                echo "head wins"
+        fi
+
+else
+        ((t++))
+        if [ $t -eq 21 ]
+        then
+        echo "tail wins"
+        fi
+fi
+done
+
+echo "head="$h"and tail="$t
+
+
